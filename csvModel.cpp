4744aa72c1c24bb3af96c039c0e5a82b895f7037
nholthaus <nholthaus@gmail.com>
2015-11-10 06:55:45 -0500

    worked on filtering

--- csvModel.cpp
+++ csvModel.cpp
@@ -143,8 +143,6 @@ bool csvModel::exportToFile(QString csvFilePath)
 //------------------------------------------------------------------------------
 csvModel* csvModel::filter(csvModel* filter)
 {
-	static int test = 0;
-	qDebug() << ++test;
 	csvModel* output = new csvModel;
 
 	// Make a hash of all the groups in the filter
@@ -167,28 +165,61 @@ csvModel* csvModel::filter(csvModel* filter)
 		size_t						group			= filter->data(filter->index(1, filterCol)).toULongLong();
 		size_t						numInGroup		= (group == 0 ? 1 : groups.count(group));
 		bool						hardHit			= filter->data(filter->index(1, filterCol), Qt::CheckStateRole).toBool();
-		QStringList	filterValues;
-
-		qDebug() << columnName;
+		QList<QStringList>			filterValues;
+		QList<QString>				filterColumnNames;
 
 		// make a list of filter values, i.e. all the things in the filter column
-		for (int filterRow = 2; filterRow < filter->rowCount(); ++filterRow)
+		auto filterColumns = groups.values(group);
+		qSort(filterColumns);
+
+		if (group != 0)
 		{
-			QString value = filter->data(filter->index(filterRow, filterCol)).toString();
-			if (!value.isEmpty()) filterValues << value;
+			// for each filter in the group
+			for (auto groupColumnItr = filterColumns.begin(); groupColumnItr != filterColumns.end(); ++groupColumnItr)
+			{
+				int column = *groupColumnItr;
+
+				filterValues.push_back(QStringList());
+				filterColumnNames.push_back(filter->headerData(column, Qt::Horizontal, Qt::DisplayRole).toString());
+
+				// erase other columns that are part of this group so we don't check them twice
+				if(groupColumnItr != filterColumns.begin()) unusedFilterColumns.erase(column);
+
+				// add all the filter values
+				for (int filterRow = 2; filterRow < filter->rowCount(); ++filterRow)
+				{
+					QString value = filter->data(filter->index(filterRow, filterCol)).toString();
+					if (!value.isEmpty()) filterValues.back() << value;
+				}
+			}
+			qDebug() << "filters:" << filterColumnNames << filterColumns;
+		}	
+		else
+		{
+			filterColumnNames.push_back(columnName);
+			qDebug() << "filters:" << filterColumnNames << filterCol;
 		}
 
 		// find the corresponding column in this spreadsheet
-		int masterColumn;
-		for (masterColumn = 0; masterColumn < this->columnCount(); masterColumn++)
+		QList<int> masterColumns;
+		QList<QString> masterColumnNames;
+		for (int i = 0; i < filterColumnNames.size(); ++i)
 		{
-			if (this->headerData(masterColumn, Qt::Horizontal, Qt::DisplayRole).toString() == columnName)
+			for (int masterColumn = 0; masterColumn < this->columnCount(); masterColumn++)
 			{
-				break;
+				QString masterColunName = this->headerData(masterColumn, Qt::Horizontal, Qt::DisplayRole).toString();
+				if (masterColunName == filterColumnNames.at(i))
+				{
+					masterColumns.push_back(masterColumn);
+					masterColumnNames.push_back(masterColunName);
+					break;
+				}
 			}
 		}
 
+		qDebug() << "master:" << masterColumnNames << masterColumns;
 		
+		//  
 	}
 
 	return output;
